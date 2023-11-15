90a7572e26a5a03e304d0e98c94bfcb555e80761
Baranouski Raman <baranouski.raman@gmail.com>
2023-11-16 00:44:39 +0300

    MiAPR lab9

--- MiAPR/Lab9/src/main/java/by/bsuir/lab9/Const.java
+++
@@ -1,9 +0,0 @@
-package by.bsuir.lab9;
-
-public interface Const {
-    int MIN_CLASSES = 2;
-    int MAX_CLASSES = 8;
-
-    int MIN_ATTRIBUTES = 3;
-    int MAX_ATTRIBUTES = 5;
-}
--- MiAPR/Lab9/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module by.bsuir.lab9 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens by.bsuir.lab9 to javafx.fxml;
-    exports by.bsuir.lab9;
-}
